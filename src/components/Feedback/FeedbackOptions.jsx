66f33e8932ba4af637a98e9c27fb72f34a7aa686
ComeAndSee-CAS <comeandsee.seo@gmail.com>
2023-05-21 18:43:20 +0300

    Up

--- src/components/Feedback/FeedbackOptions.jsx
+++ src/components/Feedback/FeedbackOptions.jsx
@@ -1,7 +1,9 @@
 export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
-  return Object.keys(options).map(feedback => (
-    <button key={feedback} type="button" onClick={onLeaveFeedback}>
-      {feedback}
-    </button>
-  ));
+  return <>
+    {Object.keys(options).map(feedback => (
+      <button key={feedback} type="button" onClick={onLeaveFeedback}>
+        {feedback}
+      </button>
+    ))}
+  </>
 };
